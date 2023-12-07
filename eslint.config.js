f9c4be4037d89b6592d58e0af88c32894670b6b2
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-12-07 18:16:24 +0900

    🔧 : eslint設定

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+// @ts-nocheck
+import js from "@eslint/js";
+import tsEsLintPlugin from "@typescript-eslint/eslint-plugin";
+import tsEsLintParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+
+const config = [
+  { ignores: ["dist"] },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "@typescript-eslint": tsEsLintPlugin,
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.cts", "**/*.mts"],
+    languageOptions: {
+      parser: tsEsLintParser,
+      parserOptions: {
+        project: true,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es6,
+      },
+    },
+    rules: {
+      ...tsEsLintPlugin.configs["eslint-recommended"].overrides[0].rules,
+      ...tsEsLintPlugin.configs["recommended-type-checked"].rules,
+    },
+  },
+];
+
+export default config;
