99f619bf8b8f7ca9827efd9c3f37d81e8a756c1b
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-12-07 18:40:45 +0900

    ♻️ : ファイルへのパスへ修正

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ const config = [
     languageOptions: {
       parser: tsEsLintParser,
       parserOptions: {
-        project: true,
+        project: "./tsconfig.json",
       },
       globals: {
         ...globals.browser,
